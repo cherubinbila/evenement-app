da2d0cb47f2c70e06bf395e6f869a5440a3bc87f
cherubinbila <bilafranck09@gmail.com>
2024-08-19 11:19:06 +0000

    seventh commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,7 +10,7 @@ import { handleError } from "@/lib/utils";
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 
-export async function createUser(user: any) {
+export async function createUser(user: CreateUserParams) {
 	try {
 		await connect();
 		const newUser = await User.create(user);
