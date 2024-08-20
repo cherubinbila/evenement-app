190794c631481e4a5ef480beb65efe8edbfad34f
cherubinbila <bilafranck09@gmail.com>
2024-08-20 17:11:37 +0000

    sixteenth

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -92,7 +92,7 @@ export async function POST(req: Request) {
 		});
 	}
 
-	if (eventType === "user.updated") {
+	/*if (eventType === "user.updated") {
 		const { id, image_url, first_name, last_name, username } = evt.data;
 
 		const user = {
@@ -105,7 +105,7 @@ export async function POST(req: Request) {
 		const updatedUser = await updateUser(id, user);
 
 		return NextResponse.json({ message: "OK", user: updatedUser });
-	}
+	}*/
 
 	if (eventType === "user.deleted") {
 		const { id } = evt.data;
